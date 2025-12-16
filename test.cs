b014dc410d2530068a53b60c4cfb2a5f6e6222be
Jade <jade16work@gmail.com>
2025-12-16 12:56:19 +0900

    Add test.cs

---
+++ test.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    static void Attack()
+    {
+        Console.WriteLine("Attack!");
+    }
+
+    static void Move()
+    {
+        Console.WriteLine("Move!");
+    }
+}
